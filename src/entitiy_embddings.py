8d04c75d6e8dfdb7e0b7bcae629c04c1cde150e0
Idan-B <Lotar707@gmail.com>
2020-07-28 18:20:53 +0300

    entitiy embddings

--- src/entitiy_embddings.py
+++ src/entitiy_embddings.py
@@ -1,15 +1,19 @@
 import os
 import gc
 import joblib
+from numpy.core.fromnumeric import shape
 import pandas as pd
 import numpy as np
 from sklearn import metrics, preprocessing
+from sklearn.utils import validation
 from tensorflow.keras import layers
 from tensorflow.keras import optimizers
 from tensorflow.keras.models import Model, load_model
 from tensorflow.keras import callbacks
 from tensorflow.keras import backend as K
 from tensorflow.keras import utils
+
+import config
 
 
 def create_model(data, catcols):
@@ -27,4 +31,119 @@ def create_model(data, catcols):
         num_unique_values = int(data[c].nunique())
         embed_dim = int(min(np.ceil((num_unique_values)/2), 50))
 
-        inp = layers
+        inp = layers.inputs(shape=(1,))
+
+        # add embedding layer to raw input
+        # embedding size is always 1 more than unique values in input
+        out  = layers.Embedding(num_unique_values + 1, embed_dim, name = c
+        )(inp)
+
+        # 1-d spatial dropout is the standard for emebedding layers
+        # you can use it in NLP task too
+        out = layers.SpatialDropout1D(0.3)(out)
+
+        # reshape the input to the dimension of embedding
+        # this becomes our output layer for current feature
+        out = layers.Reshpe(target_shap=(embed_dim, ))(out)
+
+        # add input to input list
+        inputs.append(inp)
+
+        # add output to output list
+        outputs.append(out)
+
+        # concatenate all output layers
+        x = layers.Concatenate()(outputs)
+
+        # add a batchnorm layers
+        # from now on you can try different architechtures
+        # if we have a numrical features, you shold add here
+        x = layers.BatchNormalization()(x)
+
+        x = layers.Dense(300, activation="relu")(x)
+        x = layers.Dropout(0.3)(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.Dense(300, activation="relu")(x)
+        x = layers.Dropout(0.3)(x)
+        x = layers.BatchNormalization()(x)
+
+        # using softmax and treating it as a two class problem
+        y = layers.Dense(2, activation="softmax")(x)
+
+        # create final model
+        model = Model(inputs=inputs, outputs=y)
+
+        # complie the model
+        model.complie(loss='binary_crossentropy', optimizer='adam')
+        return model
+
+
+def run(fold):
+    # load training data with folds
+    df = pd.read_csv(config.TRAINING_FILE)
+
+    features = [
+        f for f in df.columns if f not in ("id", "target", "kfold")
+    ]
+
+    # fill all NaN values with NONE
+    for col in features:
+        df.loc[:, col] = df[col].astype(str).fillna("NONE")
+
+    # encode all features with label encoder individually
+    # in live setting you need to save all label encoders
+    for feat in features:
+        lbl_enc = preprocessing.LabelEncoder()
+        df.loc[:, feat] = lbl_enc.fit_transform(df[feat].values)
+
+    # get training data using folds
+    df_train = df[df.kfold != fold].reset_index(drop=True)
+
+    # get validation data using folds
+    df_valid = df[df.kfold == fold].reset_index(drop=True)
+
+    # creat tf.keras model
+    model = create_model(df, features)
+
+    # out features are lists of lists
+    xtrain = [
+        df_train[features].values[:, k] for k in range(len(features))
+    ]
+
+    xvalid = [
+        df_valid[features].values[:, k] for k in range(len(features))
+    ]
+
+    # fetch target columns
+    ytrain = df_train.target.values
+    yvalid = df_valid.target.values
+
+    # convert target columns to categories
+    # this is just binarization
+    ytrain_cat = utils.to_categorical(ytrain)
+    yvalid_cat = utils.to_categorical(yvalid)
+
+    # fit the model
+    model.fit(xtrain,
+              ytrain_cat,
+              validation_data=(xvalid, yvalid_cat),
+              verbose=1,
+              batch_size=1024,
+              epochs=3
+    )
+
+    # genrate validation predictions
+    valid_preds = model.predict(xvalid)[:, 1]
+
+    # print roc auc score
+    print(metrics.roc_auc_score(yvalid, valid_preds))
+
+    K.clear_session()
+
+if __name__ == "__main__":
+    run(0)
+    run(1)
+    run(2)
+    run(3)
+    run(4)
---
+++ src/greedy.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+from sklearn import linear_model
+from sklearn import metrics
+from sklearn.datasets import make_classification
+
+import config
+
+
+class GreedyFeatureSelection:
+
+    def evaluate_score(self, X, y):
+
+        return
+    
+    def _feature_selection(self, X, y):
+
+        return
+
+    def __call__(self, X, y):
+
+        return
+
+if __name__ == "__main__":
