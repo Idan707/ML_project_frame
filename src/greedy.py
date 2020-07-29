da984ddeb1e985f8c63442bd0b586a61cf559746
Idan Benaun <Lotar707@gmail.com>
2020-07-29 16:08:47 +0300

    add pipeline

--- src/greedy.py
+++ src/greedy.py
@@ -8,17 +8,81 @@ import config
 
 
 class GreedyFeatureSelection:
-
+    """
+    A simple and custom class for greedy feature selection.
+    We will need to adjust to different data sets.
+    """
     def evaluate_score(self, X, y):
+        """
+        This function evaluates model on data and returns AUC.
+        We fit the data and calculate AUC on same data, means we overfitting here.
+        But this is also the way to achieve greedy selection.
 
-        return
+        If we want to implement it in really correct way,
+        Calculate OOF AUC and return mean AUC over K fold.
+
+        :param X: training data
+        :param y: targets
+        :return: overfitted area under the roc curve
+        """
+
+        # fit model and calculate auc on the same data
+        model = linear_model.LogisticRegression()
+        model.fit(X, y)
+        predictions = model.predict_proba(X)[:, 1]
+        auc = metrics.roc_auc_score(y, predictions)
+        return auc
     
     def _feature_selection(self, X, y):
+        """
+        This function dose the actual greedy selection
+        :param X: data, numpy array
+        :param y: targets, numpy array
+        :return: (best scores, best features)
+        """
+        # init good feature list
+        # and best scores to keep track of both
+        good_features = []
+        best_scores = []
 
-        return
+        # calculate the number of features
+        num_features = X.shape[1]
+
+        # infinite loop
+        while True:
+            # init best feature and score of this loop
+            this_feature = None
+            best_score = 0
+
+            # loop over all features
+            for feature in range(num_features):
+                # if feature in good_features list, skip
+                if feature in good_features:
+                    continue
+                selected_features = good_features + [feature]
+                xtrain = X[:, selected_features]
+                score = self.evaluate_score(xtrain, y)
+                # if score is greater than the best score of this loop
+                # change best score and best feature
+                if score > best_score:
+                    this_feature = feature
+                    best_score = score
+
+            if this_feature != None:
+                good_features.append(this_feature)
+                best_score.append(best_score)
+
+            if len(best_score) > 2:
+                if best_score[-1] < best_score[-2]:
+                    break
+
+        return best_score[:-1], good_features[:-1]
 
     def __call__(self, X, y):
+        scores, features = self._feature_selection(X, y)
 
-        return
+        return X[:, features], scores
 
 if __name__ == "__main__":
+    X, y = make_classification(n_samples=1000, n_features=50)
+    X_transformed, scores = GreedyFeatureSelection()(X, y)
---
+++ src/rf_gp_minimize.py
@@ -0,0 +1,105 @@
+import numpy as np
+import pandas as pd
+
+from functools import partial
+
+from sklearn import ensemble
+from sklearn import metrics 
+from sklearn import model_selection
+
+from skopt import gp_minimize
+from skopt import space
+
+import config
+
+
+def optimize(params, param_names, x, y):
+    """
+    The main optimization function.
+    This function takes all the arguments from the search space
+    and training features and targets. It then initializes
+    the models by setting the chosen parameters and runs cv 
+    and return negative accuracy score
+
+    :param params: list of params from gp_minimize
+    :param param_names: list of param names. order is important!
+    :param x: training data
+    :param y: labels/targets
+    :return: negative accuracy after 5 folds
+    """
+    params = dict(zip(param_names, params))
+    model = ensemble.RandomForestClassifier(**params)
+    kf = model_selection.StratifiedKFold(n_splits=5)
+
+    accuracies = []
+
+    for idx in kf.split(X=x, y=y):
+        train_idx, test_idx = idx[0], idx[1]
+        xtrain = x[train_idx]
+        ytrain = y[train_idx]
+
+        xtest = x[test_idx]
+        ytest = y[test_idx]
+
+        model.fit(xtrain, ytrain)
+
+        preds = model.predict(xtest)
+
+        fold_accuracy = metrics.accuracy_score(
+            ytest,
+            preds
+        )
+        accuracies.append(fold_accuracy)
+
+    return -1 * np.mean(accuracies)
+
+
+if __name__ == "__main__":
+    df = pd.read_csv(config.TRAINING_FILE)
+
+    X = df.drop("price_range", axis=1).values
+    y = df.price_range.values
+
+    param_space = [
+        space.Integer(3, 15, name="max_depth"),
+        space.Integer(100, 1500, name="n_estimators"),
+        space.Categorical(["gini", "entropy"], name="criterion"),
+        space.Real(0.01, 1, prior="uniform", name="max_features")
+    ]
+
+    param_names = [
+        "max_depth",
+        "n_estimators",
+        "criterion",
+        "max_features"
+    ]
+
+    # by using functools partial, we create a new function
+    # which has same parameters as the optimize function
+    # except for the fact that only one param is required
+    # this is how gp_minimize expects the optimization function to be.
+    # you can change it by reading data inside the optimize function or by 
+    # defining the optimize functions here 
+    optimization_function = partial(
+        optimize,
+        param_names=param_names,
+        x=X,
+        y=y
+    )
+
+    result = gp_minimize(
+        optimization_function,
+        dimensions=param_space,
+        n_calls=15,
+        n_random_starts=10,
+        verbose=10
+    )
+
+    best_params = dict(
+        zip(
+            param_names,
+            result.x
+        )
+    )
+
+    print(best_params)
